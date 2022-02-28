3c6fd0c2e11f6585b46e42703c507e9c84bd9284
Darel Martínez <cls18drl@gmail.com>
2022-02-28 15:15:15 +0000

    first commit

---
+++ Tut_Darel_Martinez_Caballero/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace Tut_Darel_Martinez_Caballero
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExaminar = new System.Windows.Forms.Button();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.btnDestino = new System.Windows.Forms.Button();
+            this.btnRenombrar = new System.Windows.Forms.Button();
+            this.lbElementos = new System.Windows.Forms.ListBox();
+            this.cbEnviar = new System.Windows.Forms.ComboBox();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnMover = new System.Windows.Forms.Button();
+            this.btnCrear = new System.Windows.Forms.Button();
+            this.rdBtnFichero = new System.Windows.Forms.RadioButton();
+            this.rdBtnCarpeta = new System.Windows.Forms.RadioButton();
+            this.btnListar = new System.Windows.Forms.Button();
+            this.txtRutaExaminar = new System.Windows.Forms.TextBox();
+            this.txtRutaDestino = new System.Windows.Forms.TextBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.txtRenombrar = new System.Windows.Forms.TextBox();
+            this.chkLongitud = new System.Windows.Forms.CheckBox();
+            this.chkUltimoAcceso = new System.Windows.Forms.CheckBox();
+            this.chkGuardar = new System.Windows.Forms.CheckBox();
+            this.richTextDocumentos = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnExaminar
+            // 
+            this.btnExaminar.Location = new System.Drawing.Point(59, 39);
+            this.btnExaminar.Name = "btnExaminar";
+            this.btnExaminar.Size = new System.Drawing.Size(75, 23);
+            this.btnExaminar.TabIndex = 0;
+            this.btnExaminar.Text = "Examinar";
+            this.btnExaminar.UseVisualStyleBackColor = true;
+            this.btnExaminar.Click += new System.EventHandler(this.btnExaminar_Click);
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(59, 145);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(75, 23);
+            this.btnEnviar.TabIndex = 1;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // btnDestino
+            // 
+            this.btnDestino.Location = new System.Drawing.Point(59, 84);
+            this.btnDestino.Name = "btnDestino";
+            this.btnDestino.Size = new System.Drawing.Size(75, 23);
+            this.btnDestino.TabIndex = 2;
+            this.btnDestino.Text = "Destino";
+            this.btnDestino.UseVisualStyleBackColor = true;
+            this.btnDestino.Click += new System.EventHandler(this.btnDestino_Click);
+            // 
+            // btnRenombrar
+            // 
+            this.btnRenombrar.Location = new System.Drawing.Point(14, 214);
+            this.btnRenombrar.Name = "btnRenombrar";
+            this.btnRenombrar.Size = new System.Drawing.Size(100, 23);
+            this.btnRenombrar.TabIndex = 3;
+            this.btnRenombrar.Text = "Renombrar";
+            this.btnRenombrar.UseVisualStyleBackColor = true;
+            this.btnRenombrar.Click += new System.EventHandler(this.btnRenombrar_Click);
+            // 
+            // lbElementos
+            // 
+            this.lbElementos.FormattingEnabled = true;
+            this.lbElementos.ItemHeight = 16;
+            this.lbElementos.Location = new System.Drawing.Point(120, 214);
+            this.lbElementos.Name = "lbElementos";
+            this.lbElementos.Size = new System.Drawing.Size(169, 148);
+            this.lbElementos.TabIndex = 4;
+            this.lbElementos.SelectedIndexChanged += new System.EventHandler(this.lbElementos_SelectedIndexChanged);
+            this.lbElementos.DoubleClick += new System.EventHandler(this.doublClickSelectItem);
+            // 
+            // cbEnviar
+            // 
+            this.cbEnviar.FormattingEnabled = true;
+            this.cbEnviar.Location = new System.Drawing.Point(153, 145);
+            this.cbEnviar.Name = "cbEnviar";
+            this.cbEnviar.Size = new System.Drawing.Size(121, 24);
+            this.cbEnviar.TabIndex = 5;
+            this.cbEnviar.SelectedIndexChanged += new System.EventHandler(this.cbEnviar_SelectedIndexChanged);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(214, 185);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 6;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnMover
+            // 
+            this.btnMover.Location = new System.Drawing.Point(120, 185);
+            this.btnMover.Name = "btnMover";
+            this.btnMover.Size = new System.Drawing.Size(75, 23);
+            this.btnMover.TabIndex = 7;
+            this.btnMover.Text = "Mover";
+            this.btnMover.UseVisualStyleBackColor = true;
+            this.btnMover.Click += new System.EventHandler(this.btnMover_Click);
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.Location = new System.Drawing.Point(444, 64);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(75, 23);
+            this.btnCrear.TabIndex = 8;
+            this.btnCrear.Text = "Crear";
+            this.btnCrear.UseVisualStyleBackColor = true;
+            this.btnCrear.Click += new System.EventHandler(this.btnCrear_Click);
+            // 
+            // rdBtnFichero
+            // 
+            this.rdBtnFichero.AutoSize = true;
+            this.rdBtnFichero.Location = new System.Drawing.Point(525, 53);
+            this.rdBtnFichero.Name = "rdBtnFichero";
+            this.rdBtnFichero.Size = new System.Drawing.Size(73, 20);
+            this.rdBtnFichero.TabIndex = 9;
+            this.rdBtnFichero.TabStop = true;
+            this.rdBtnFichero.Text = "Fichero";
+            this.rdBtnFichero.UseVisualStyleBackColor = true;
+            // 
+            // rdBtnCarpeta
+            // 
+            this.rdBtnCarpeta.AutoSize = true;
+            this.rdBtnCarpeta.Location = new System.Drawing.Point(525, 79);
+            this.rdBtnCarpeta.Name = "rdBtnCarpeta";
+            this.rdBtnCarpeta.Size = new System.Drawing.Size(76, 20);
+            this.rdBtnCarpeta.TabIndex = 10;
+            this.rdBtnCarpeta.TabStop = true;
+            this.rdBtnCarpeta.Text = "Carpeta";
+            this.rdBtnCarpeta.UseVisualStyleBackColor = true;
+            this.rdBtnCarpeta.CheckedChanged += new System.EventHandler(this.rdBtnCarpeta_CheckedChanged);
+            // 
+            // btnListar
+            // 
+            this.btnListar.Location = new System.Drawing.Point(183, 13);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(75, 23);
+            this.btnListar.TabIndex = 12;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = true;
+            this.btnListar.Click += new System.EventHandler(this.btnListar_Click);
+            // 
+            // txtRutaExaminar
+            // 
+            this.txtRutaExaminar.Location = new System.Drawing.Point(153, 43);
+            this.txtRutaExaminar.Name = "txtRutaExaminar";
+            this.txtRutaExaminar.Size = new System.Drawing.Size(285, 22);
+            this.txtRutaExaminar.TabIndex = 13;
+            // 
+            // txtRutaDestino
+            // 
+            this.txtRutaDestino.Location = new System.Drawing.Point(153, 84);
+            this.txtRutaDestino.Name = "txtRutaDestino";
+            this.txtRutaDestino.Size = new System.Drawing.Size(285, 22);
+            this.txtRutaDestino.TabIndex = 14;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(525, 368);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(224, 23);
+            this.btnGuardar.TabIndex = 15;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // txtRenombrar
+            // 
+            this.txtRenombrar.Location = new System.Drawing.Point(14, 243);
+            this.txtRenombrar.Name = "txtRenombrar";
+            this.txtRenombrar.Size = new System.Drawing.Size(100, 22);
+            this.txtRenombrar.TabIndex = 16;
+            // 
+            // chkLongitud
+            // 
+            this.chkLongitud.AutoSize = true;
+            this.chkLongitud.Checked = true;
+            this.chkLongitud.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkLongitud.Location = new System.Drawing.Point(632, 15);
+            this.chkLongitud.Name = "chkLongitud";
+            this.chkLongitud.Size = new System.Drawing.Size(149, 20);
+            this.chkLongitud.TabIndex = 17;
+            this.chkLongitud.Text = "Longitud del archivo";
+            this.chkLongitud.UseVisualStyleBackColor = true;
+            // 
+            // chkUltimoAcceso
+            // 
+            this.chkUltimoAcceso.AutoSize = true;
+            this.chkUltimoAcceso.Checked = true;
+            this.chkUltimoAcceso.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkUltimoAcceso.Location = new System.Drawing.Point(632, 45);
+            this.chkUltimoAcceso.Name = "chkUltimoAcceso";
+            this.chkUltimoAcceso.Size = new System.Drawing.Size(167, 20);
+            this.chkUltimoAcceso.TabIndex = 18;
+            this.chkUltimoAcceso.Text = "Hora del último acceso";
+            this.chkUltimoAcceso.UseVisualStyleBackColor = true;
+            // 
+            // chkGuardar
+            // 
+            this.chkGuardar.AutoSize = true;
+            this.chkGuardar.Location = new System.Drawing.Point(632, 79);
+            this.chkGuardar.Name = "chkGuardar";
+            this.chkGuardar.Size = new System.Drawing.Size(150, 20);
+            this.chkGuardar.TabIndex = 19;
+            this.chkGuardar.Text = "Guardar Resultados";
+            this.chkGuardar.UseVisualStyleBackColor = true;
+            // 
+            // richTextDocumentos
+            // 
+            this.richTextDocumentos.Location = new System.Drawing.Point(444, 145);
+            this.richTextDocumentos.Name = "richTextDocumentos";
+            this.richTextDocumentos.Size = new System.Drawing.Size(337, 217);
+            this.richTextDocumentos.TabIndex = 21;
+            this.richTextDocumentos.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextDocumentos);
+            this.Controls.Add(this.chkGuardar);
+            this.Controls.Add(this.chkUltimoAcceso);
+            this.Controls.Add(this.chkLongitud);
+            this.Controls.Add(this.txtRenombrar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.txtRutaDestino);
+            this.Controls.Add(this.txtRutaExaminar);
+            this.Controls.Add(this.btnListar);
+            this.Controls.Add(this.rdBtnCarpeta);
+            this.Controls.Add(this.rdBtnFichero);
+            this.Controls.Add(this.btnCrear);
+            this.Controls.Add(this.btnMover);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.cbEnviar);
+            this.Controls.Add(this.lbElementos);
+            this.Controls.Add(this.btnRenombrar);
+            this.Controls.Add(this.btnDestino);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.btnExaminar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExaminar;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.Button btnDestino;
+        private System.Windows.Forms.Button btnRenombrar;
+        private System.Windows.Forms.ListBox lbElementos;
+        private System.Windows.Forms.ComboBox cbEnviar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnMover;
+        private System.Windows.Forms.Button btnCrear;
+        private System.Windows.Forms.RadioButton rdBtnFichero;
+        private System.Windows.Forms.RadioButton rdBtnCarpeta;
+        private System.Windows.Forms.Button btnListar;
+        private System.Windows.Forms.TextBox txtRutaExaminar;
+        private System.Windows.Forms.TextBox txtRutaDestino;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.TextBox txtRenombrar;
+        private System.Windows.Forms.CheckBox chkLongitud;
+        private System.Windows.Forms.CheckBox chkUltimoAcceso;
+        private System.Windows.Forms.CheckBox chkGuardar;
+        private System.Windows.Forms.RichTextBox richTextDocumentos;
+    }
+}
+
---
+++ Tut_Darel_Martinez_Caballero/Form1.cs
@@ -0,0 +1,361 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tut_Darel_Martinez_Caballero
+{
+    //Darel Martínez Caballero
+
+    public partial class Form1 : Form
+    {
+        private String nombreArchivo;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            writeFile(this.richTextDocumentos.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtRutaExaminar.Text = Directory.GetCurrentDirectory();
+            txtRutaDestino.Text = Directory.GetCurrentDirectory();
+            txtRutaDestino.Text = Directory.GetCurrentDirectory();
+            actualizar_lista(txtRutaExaminar.Text);
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            System.IO.FileInfo thisFile = new FileInfo(this.cbEnviar.SelectedItem.ToString());
+            String NewPath = " NewPath holds the path the user has entered.";
+            NewPath = txtRutaExaminar.Text;
+
+            string texto = "Los atributos del fichero " + this.cbEnviar.SelectedItem.ToString() + " son : ";
+            if (this.chkLongitud.Checked)
+            {
+                texto = texto + "La longitud del fichero es: " + thisFile.Length.ToString();
+            }
+
+            if (this.chkUltimoAcceso.Checked)
+            {
+                texto = texto + " última modificación fue: " + thisFile.LastAccessTime.ToString();
+            }
+
+            if (this.chkGuardar.Checked)
+            {
+                writeFile(texto);
+            }
+
+            MessageBox.Show(texto);
+            
+        }
+
+        private void rdBtnCarpeta_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExaminar_Click(object sender, EventArgs e)
+        {
+            String NewPath = " NewPath holds the path the user has entered.";
+            String ErrorMessage = null;
+            NewPath = this.txtRutaExaminar.Text;
+            lbElementos.Items.Clear();
+            lbElementos.Refresh();
+            // Change the location to NewPath.
+            try
+            {
+                Directory.SetCurrentDirectory(NewPath);
+                String sourceDirectory = NewPath;
+                this.cbEnviar.Items.Clear();
+                this.cbEnviar.Refresh();
+                var txtFiles = Directory.EnumerateFiles(sourceDirectory);
+                foreach (String currentFile in txtFiles)
+                {
+                    String fileName = currentFile.Substring(sourceDirectory.Length + 1);
+                    this.cbEnviar.Items.Add(fileName);
+                }
+
+
+                //Listar archivos y carpetas
+                String rutaDirectorio = String.Empty;
+
+                FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    rutaDirectorio = fbd.SelectedPath;
+                }
+
+                this.txtRutaExaminar.Text = rutaDirectorio;
+
+                actualizar_lista(rutaDirectorio);
+            }
+            catch (DirectoryNotFoundException f)
+            {
+                ErrorMessage = "You must enter a valid path. If trying to access a different drive, remember to include the drive letter.";
+            }
+            catch
+            { //' This checks to make sure the path is not blank.
+                ErrorMessage = "You must enter a path.";
+            }
+            finally
+            {
+                //' Display the error message only if one exists.
+                if (ErrorMessage != null)
+                    MessageBox.Show(ErrorMessage);
+            }
+
+        }
+
+        private void lbElementos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.nombreArchivo = "";
+
+            try
+            {
+                this.nombreArchivo = this.lbElementos.SelectedItem.ToString();
+            }
+            catch (Exception exception) { }
+        }
+
+        private void btnMover_Click(object sender, EventArgs e)
+        {
+            String posicionInicial = this.txtRutaExaminar.Text;
+            String posicionFinal = this.txtRutaDestino.Text;
+
+            if (File.Exists(posicionInicial + "\\" + this.nombreArchivo))
+            {
+
+                System.IO.File.Move(posicionInicial + "\\" + this.nombreArchivo, posicionFinal + "\\" + this.nombreArchivo);
+
+            }
+            else if (Directory.Exists(posicionInicial + "\\" + this.nombreArchivo))
+            {
+
+                System.IO.Directory.Move(posicionInicial + "\\" + this.nombreArchivo, posicionFinal + "\\" + this.nombreArchivo);
+
+            }
+            else
+            {
+
+                MessageBox.Show("No existe");
+
+            }
+
+            actualizar_lista(posicionInicial);
+
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            actualizar_lista(this.txtRutaExaminar.Text);
+        }
+
+        private void actualizar_lista(String directory) {
+            this.lbElementos.Items.Clear();
+            this.lbElementos.Refresh();
+            try
+            {
+                if (directory.Trim() != String.Empty)
+                {
+                    DirectoryInfo di = new DirectoryInfo(directory);
+
+                    foreach (var item in di.GetFiles())
+                    {
+                        this.lbElementos.Items.Add(item.Name);
+                    }
+
+                    foreach (var direcotory in di.GetDirectories())
+                    {
+                        this.lbElementos.Items.Add(direcotory.Name);
+                    }
+                }
+            }
+            catch (Exception ex) { }
+        }
+        public void writeFile(String result)
+        {
+            String contenido = result;
+
+            
+
+            StreamWriter sw = new StreamWriter(this.txtRutaExaminar.Text + "\\" + this.nombreArchivo);
+
+            sw.WriteLine(contenido);
+            sw.Close();
+
+            this.richTextDocumentos.Clear();
+            this.lbElementos.Refresh();
+            MessageBox.Show("Fichero guardado");
+        }
+
+        public void readFile()
+        {
+            try
+            {
+                if (File.Exists(this.txtRutaExaminar.Text + "\\" + this.nombreArchivo))
+                {
+                    StreamReader sr = new StreamReader(this.txtRutaExaminar.Text + "\\" + this.nombreArchivo);
+
+                    String lineas; ;
+
+                    String contenido = "";
+                    while ((lineas = sr.ReadLine()) != null)
+                    {
+                        contenido += lineas;
+                    }
+
+                    this.richTextDocumentos.Text = contenido;
+                    sr.Close();
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageBox.Show("El archivo no se encuentra");
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            String nombreArchivo = this.lbElementos.SelectedItem.ToString();
+            String rutaArchivo = this.txtRutaExaminar.Text + "\\" + nombreArchivo;
+
+            if (Directory.Exists(rutaArchivo))
+            {
+                System.IO.DirectoryInfo di = new DirectoryInfo(@rutaArchivo);
+
+                foreach (FileInfo file in di.GetFiles())
+                {
+                    file.Delete();
+                }
+                foreach (DirectoryInfo dir in di.GetDirectories())
+                {
+                    dir.Delete(true);
+                }
+
+                di.Delete();
+            }
+            if (File.Exists(rutaArchivo))
+            {
+                File.Delete(rutaArchivo);
+            }
+            this.actualizar_lista(this.txtRutaExaminar.Text);
+        }
+
+        private void btnDestino_Click(object sender, EventArgs e)
+        {
+            String rutaFinal = this.txtRutaExaminar.Text + "\\" + this.txtRenombrar.Text;
+            try
+            {
+                if (rdBtnCarpeta.Checked)
+                {
+                    if (!Directory.Exists(rutaFinal))
+                    {
+                        MessageBox.Show(rutaFinal);
+                    }
+                }
+                else
+                {
+                    if (!File.Exists(rutaFinal))
+                    {
+                        File.Create(rutaFinal);
+                    }
+                }
+            }
+            catch (DirectoryNotFoundException ex) { }
+            actualizar_lista(this.txtRutaExaminar.Text);
+        }
+
+        private void btnRenombrar_Click(object sender, EventArgs e)
+        {
+            String directory = this.txtRutaExaminar.Text;
+            String dirCopy = directory + "\\" + this.nombreArchivo;
+            String newNamePath = directory + "\\" + this.txtRenombrar.Text;
+            MessageBox.Show(newNamePath);
+
+            string path = @"C:\";
+
+
+
+            if (File.Exists(path))
+
+            {
+                System.IO.File.Move(dirCopy, newNamePath);
+            }
+
+            else if (Directory.Exists(path))
+
+            {
+                System.IO.Directory.Move(dirCopy, newNamePath);
+            }
+
+            else
+
+            {
+
+                MessageBox.Show("No existe");
+
+            }
+
+            actualizar_lista(directory);
+        }
+
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+            String rutaFinal = this.txtRutaExaminar.Text + "\\" + this.txtRenombrar.Text;
+            try
+            {
+                if (rdBtnCarpeta.Checked)
+                {
+                    if (!Directory.Exists(rutaFinal))
+                    {
+                        MessageBox.Show(rutaFinal);
+                        Directory.CreateDirectory(rutaFinal);
+
+                    }
+                }
+                else
+                {
+                    if (!File.Exists(rutaFinal))
+                    {
+                        File.Create(rutaFinal);
+                    }
+                }
+            }
+            catch (DirectoryNotFoundException ex) { }
+            actualizar_lista(this.txtRutaExaminar.Text);
+        }
+
+        private void doublClickSelectItem(object sender, EventArgs e)
+        {
+            String rutaActual = this.txtRutaExaminar.Text;
+            if (File.Exists(rutaActual+"\\"+this.nombreArchivo))
+            {
+                MessageBox.Show("hola");
+                readFile();
+            }else if (Directory.Exists(rutaActual + "\\" + this.nombreArchivo))
+            {
+                this.actualizar_lista(rutaActual + "\\" + this.nombreArchivo);
+                this.txtRutaExaminar.Text = rutaActual + "\\" + this.nombreArchivo;
+            }
+            
+        }
+
+        private void cbEnviar_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
